6099da45010824b97fceb2affd7f24d88d7bbabf
Derek Wheelden <derek.wheelden@gmail.com>
2014-01-02 21:34:58 -0500

    Basic setup to render views

---
+++ views/layout.mjs
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>{{title}}</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|Oswald' rel='stylesheet' type='text/css'>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>It&rsquo;s a fuckin&rsquo; page, asshole.</h1>
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    </body>
+</html>
